614c514d9b825aa12139a6e50f8f60a1eb80d837
Thu Quoc Nguyen <thuqnguyen@kms-technology.com>
2018-11-28 09:59:33 +0700

    define models

---
+++ models/genre.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const GenreSchema = new Schema({
+  name: { type: String, min: 3, max: 100, required: true }
+});
+
+GenreSchema.virtual('url').get(() => `/catalog/genre/${this._id}`);
+
+module.exports = mongoose.model('Genre', GenreSchema);
