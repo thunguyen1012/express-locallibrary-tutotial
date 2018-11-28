482abb6c70ded8ffcd11232480b5dd58b7b219c9
Thu Quoc Nguyen <thuqnguyen@kms-technology.com>
2018-11-28 19:33:37 +0700

    book instance list

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const moment = require('moment');
 const Schema = mongoose.Schema;
 
 const BookInstanceSchema = new Schema({
@@ -15,6 +16,10 @@ const BookInstanceSchema = new Schema({
 
 BookInstanceSchema.virtual('url').get(function() {
   return `/catalog/bookinstance/${this.id}`;
+});
+
+BookInstanceSchema.virtual('due_back_formatted').get(function() {
+  return moment(this.due_back).format('MMMM Do, YYYY');
 });
 
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
