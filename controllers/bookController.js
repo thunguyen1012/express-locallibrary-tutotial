a6f1c5f86943ad34395f40c59a1202df97e2ed7c
Thu Quoc Nguyen <thuqnguyen@kms-technology.com>
2018-11-28 19:03:40 +0700

    home page with real data

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,7 +1,32 @@
+const async = require('async');
 const Book = require('../models/book');
+const Author = require('../models/author');
+const Genre = require('../models/genre');
+const BookInstance = require('../models/bookinstance');
 
 exports.index = function(req, res) {
-  res.send('Site Home Page');
+  async.parallel(
+    {
+      book_count: callback => {
+        Book.countDocuments({}, callback);
+      },
+      book_instance_count: callback => {
+        BookInstance.countDocuments({}, callback);
+      },
+      book_instance_available_count: callback => {
+        BookInstance.countDocuments({ status: 'Available' }, callback);
+      },
+      author_count: callback => {
+        Author.countDocuments({}, callback);
+      },
+      genre_count: callback => {
+        Genre.countDocuments({}, callback);
+      }
+    },
+    (err, results) => {
+      res.render('index', { title: 'Local Library Home', error: err, data: results });
+    }
+  );
 };
 
 // Display list of all books.
