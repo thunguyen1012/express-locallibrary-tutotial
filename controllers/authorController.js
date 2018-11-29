d65fd1a541b7fda3d5ffe4f7136a1b80ccf74c52
Thu Quoc Nguyen <thuqnguyen@kms-technology.com>
2018-11-29 18:54:59 +0700

    update author

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -181,11 +181,77 @@ exports.author_delete_post = (req, res, next) => {
 };
 
 // Display Author update form on GET.
-exports.author_update_get = (req, res) => {
-  res.send('Author update GET');
+exports.author_update_get = (req, res, next) => {
+  Author.findById(req.params.id, (err, result) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (result === null) {
+      err = new Error('Author not found');
+      err.status = 404;
+      return next(err);
+    }
+
+    res.render('author_form', { title: 'Update Author', author: result });
+  });
 };
 
 // Handle Author update on POST.
-exports.author_update_post = (req, res) => {
-  res.send('Author update POST');
-};
+exports.author_update_post = [
+  // Validate fields.
+  body('first_name')
+    .isLength({ min: 1 })
+    .trim()
+    .withMessage('First name must be specified.')
+    .isAlphanumeric()
+    .withMessage('First name has non-alphanumeric characters.'),
+  body('family_name')
+    .isLength({ min: 1 })
+    .trim()
+    .withMessage('Family name must be specified.')
+    .isAlphanumeric()
+    .withMessage('Family name has non-alphanumeric characters.'),
+  body('date_of_birth', 'Invalid date of birth')
+    .optional({ checkFalsy: true })
+    .isISO8601(),
+  body('date_of_death', 'Invalid date of death')
+    .optional({ checkFalsy: true })
+    .isISO8601(),
+
+  // Sanitize fields.
+  sanitizeBody('first_name')
+    .trim()
+    .escape(),
+  sanitizeBody('family_name')
+    .trim()
+    .escape(),
+  sanitizeBody('date_of_birth').toDate(),
+  sanitizeBody('date_of_death').toDate(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request .
+    const errors = validationResult(req);
+
+    const author = new Author({
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body.date_of_death,
+      _id: req.params.id
+    });
+
+    if (!errors.isEmpty()) {
+      res.render('author_form', { title: 'Update Author', author: author, errors: errors.array() });
+      return;
+    } else {
+      Genre.findByIdAndUpdate(req.params.id, author, {}, function(err, result) {
+        if (err) {
+          return next(err);
+        }
+        res.redirect(result.url);
+      });
+    }
+  }
+];
--- models/author.js
+++ models/author.js
@@ -31,4 +31,14 @@ AuthorSchema.virtual('url').get(function() {
   return '/catalog/author/' + this._id;
 });
 
+AuthorSchema.virtual('date_of_birth_yyyy_mm_dd').get(function () {
+  if (!this.date_of_birth) return this.date_of_birth;
+  return moment(this.date_of_birth).format('YYYY-MM-DD');
+});
+
+AuthorSchema.virtual('date_of_death_yyyy_mm_dd').get(function () {
+  if (!this.date_of_death) return this.date_of_death;
+  return moment(this.date_of_death).format('YYYY-MM-DD');
+});
+
 module.exports = mongoose.model('Author', AuthorSchema);
